16772438e87f08e0e1cae6248bf8c25ce46a4213
Docker <docker@example.com>
2024-10-16 14:00:50 +0100

    pisello

--- basics/recognizer/lib/recognizer.ml
+++ basics/recognizer/lib/recognizer.ml
@@ -1,10 +1,29 @@
-let lang1 _ = failwith ""
+let rec lang1 l1 = match l1 with
+[] -> false
+|[a] -> if(a = 0 || a = 1) then true else false
+|a::tl -> if(a = 0 || a = 1) then lang1 tl else false
 
-let lang2 _ = failwith ""
+let rec lang2 l2 = match l2 with
+[] -> true
+|['1'] -> lang2 tl
+|['0'] -> List.for_all(fun x -> x <> '0') tl
+|_ -> false
 
-let lang3 _ = failwith ""
 
-let lang4 _ = failwith ""
+
+let rec aux  l = match l with
+[] -> false
+|['0'] -> true
+|_::tl -> aux tl
+
+
+let rec lang3 l3 = match l3 with
+[] -> false
+|0::tl -> if(aux tl) then List.for_all(fun x -> x = 1 || x = 0) tl else false 
+|_ -> false
+
+let lang4 l4 = ((List.fold_left(fun aux2 x -> if(x = 1) then aux2 +1 else aux2) 0 l4 ) = 2)
+ && (List.for_all(fun x -> x = 1 || x = 0 ) l4)
 
 let lang5 _ = failwith ""
     
